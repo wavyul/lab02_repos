bbcf5965f602de994eced3f08f9622488b01f9b9
wavyul <wmmmrww@gmail.com>
2024-09-15 21:46:34 +0300

    first master commit

---
+++ LB2.0/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double Pi = 4 * atan(1.);
+	double a;
+	double z1;
+	// double z2;
+
+	cout << "a = "; cin >> a;
+
+	z1 = sin((Pi / 2.) + (3 * a)) / (1 - (sin((3 * a) - Pi)));
+	// z2 = 1. / tan((5 * Pi / 4.) + (3 * a / 2.));
+
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	// cout << "z2 = " << z2 << endl;
+
+	cin.get();
+
+	return 0;
+}
